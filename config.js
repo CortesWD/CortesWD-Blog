fa4f778203c0a56618e6824891d95cd439186578
CortesWD <cccp.designer@gmail.com>
2014-08-30 13:22:05 -0500

    config dev

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://corteswd.com.co/',
+        url: 'http://corteswd-blog.herokuapp.com/',
         mail: {},
         database: {
             client: 'postgres',
