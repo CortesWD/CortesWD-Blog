ea16117d1c6c4b622b7775fb287f4ff4eaf22242
CortesWD <cccp.designer@gmail.com>
2014-08-30 19:09:39 -0500

    domain added

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://corteswd-blog.herokuapp.com/',
+        url: 'http://corteswd.com.co/',
         mail: {},
         database: {
             client: 'postgres',
